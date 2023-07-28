eea919a07f2e7856fc70bb6f590c76809e912538
JuJu222 <64346619+JuJu222@users.noreply.github.com>
2023-07-28 09:41:39 +0700

    Initial Commit

---
+++ NutriCam/NutriCamApp.swift
@@ -0,0 +1,17 @@
+//
+//  NutriCamApp.swift
+//  NutriCam
+//
+//  Created by Yap Justin on 28/07/23.
+//
+
+import SwiftUI
+
+@main
+struct NutriCamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
