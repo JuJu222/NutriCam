d598541aa5b399954a0098af4fa87e16b84c89d9
Maximus Aurelius Wiranata <83909501+maxx2002@users.noreply.github.com>
2023-08-05 15:09:33 +0700

    Dummy Statistics Chart

--- NutriCam/View/StatisticsView.swift
+++ NutriCam/View/StatisticsView.swift
@@ -6,13 +6,40 @@
 //
 
 import SwiftUI
+import Charts
 
 struct StatisticsView: View {
+    
+    @State var chartOption = 0
+    
     var body: some View {
         NavigationStack {
             VStack {
+//                Chart(contents) { content in
+                Chart {
+                    if chartOption == 0 {
+                        BarMark(x: .value("Date", "01 August"), y: .value("Value", 2000)
+                        )
+                        .cornerRadius(4)
+                        BarMark(x: .value("Date", "02 August"), y: .value("Value", 1000)
+                        )
+                        .cornerRadius(4)
+                    } else {
+                        LineMark(x: .value("Date", "01 August"), y: .value("Value", 2000)
+                        )
+                        PointMark(x: .value("Date", "01 August"), y: .value("Value", 2000)
+                        )
+                        LineMark(x: .value("Date", "02 August"), y: .value("Value", 1000)
+                        )
+                        PointMark(x: .value("Date", "02 August"), y: .value("Value", 1000)
+                        )
+                    }
+                }
+                .frame(maxHeight: 300)
                 
+                Spacer()
             }
+            .padding()
             .background(Color("Background"))
             .navigationTitle("Statistics")
         }
