59e64ab021672f8008575abff7ff94e8db40a49d
Jeremy Ernesto <jeremyernestosw@gmail.com>
2023-08-16 11:58:29 +0700

    Added notification

--- NutriCam/View/ContentView.swift
+++ NutriCam/View/ContentView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import UserNotifications
 
 struct ContentView: View {
     var body: some View {
@@ -25,6 +26,56 @@ struct ContentView: View {
                     Label("Profile", systemImage: "person.fill")
                 }
         }
+        .onAppear {
+            scheduleNotifications()
+        }
+    }
+    
+    func scheduleNotifications() {
+        let notificationTimes: [DateComponents] = [
+            makeDateComponents(hour: 6),
+            makeDateComponents(hour: 12),
+            makeDateComponents(hour: 18)
+        ]
+        
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { granted, error in
+            if granted {
+                print("Notification permission granted")
+                
+                for components in notificationTimes {
+                    let trigger = UNCalendarNotificationTrigger(dateMatching: components, repeats: true)
+                    let content = UNMutableNotificationContent()
+                    if components.hour == 6 {
+                        content.title = "It's time for breakfast!"
+                    } else if components.hour == 12 {
+                        content.title = "It's time for lunch!"
+                    } else if components.hour == 18 {
+                        content.title = "It's time for dinner!"
+                    }
+                    content.body = "Don't forget to add your food"
+                    content.sound = UNNotificationSound.default
+                    
+                    let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+                    UNUserNotificationCenter.current().add(request) {
+                        error in
+                        if let error = error {
+                            print("Error scheduling notification:\(error)")
+                        } else {
+                            print("Notifications scheduled successfully")
+                        }
+                    }
+                }
+            } else if let error = error {
+                print("Error requesting notification permission:\(error)")
+            }
+        }
+    }
+        
+    func makeDateComponents(hour: Int) -> DateComponents {
+        var dateComponents = DateComponents()
+        dateComponents.hour = hour
+        dateComponents.minute = 0
+        return dateComponents
     }
 }
 
